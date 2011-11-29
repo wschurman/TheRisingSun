bb6f5eb4dc85c50dcb32be2196aff49e51580461
wschurman <wschurman@gmail.com>
2011-11-29 11:00:04 -0500

    Merge tcat-ios into master

--- Resources/textRoute.js
+++ Resources/textRoute.js
@@ -4,61 +4,52 @@ var userStartingPoint = win.from;
 var userDestination = win.to;
 var data = win.data;
 
-var scrollView = Titanium.UI.createScrollView({
-	backgroundColor: '#FFF',
-	contentWidth:"100%",
-	contentHeight:"auto",
-	top:0,
-	width: "100%",
-	height: "100%",
-	showVerticalScrollIndicator:true,
-	showHorizontalScrollIndicator:false,
+var table = Ti.UI.createTableView({
+	top: 0
 });
 
-var textWrap = Ti.UI.createView({ 
-	backgroundColor: '#FFF', 
-	height: 'auto', 
-	width: 'auto',
-	left:5,
-	top:10,
-	layout:'vertical'
-});
+var row;
+var tableData = [];
 
 // Create the textual directions
 for (var k = 0; k < data.directions.length; k++) {
+	row = Ti.UI.createTableViewRow({
+            height: 60,
+            backgroundColor: (k % 2 == 1) ? "#E2E4EA" : "#fff"
+            //hasChild:true,
+            //hasDetail:(i < 2) // for future unread icons
+        });
+	
+	var view = Titanium.UI.createView({
+	});
+	
+	var num = Titanium.UI.createLabel({
+		color:"#333",
+		font:{
+			fontSize:20
+		},
+		text:k+1,
+		left: 10,
+		width: 25,
+		textAlign:"center"
+	});
+	
 	var label = Titanium.UI.createLabel({
 		color:'#111',
 		font: {
-			fontFamily:'Georgia',
-			fontSize:20
+			fontSize:15
 		},		
-		text:data.directions[k] + "\n"
+		text:data.directions[k].direction,
+		top:0,
+		left: 45,
+		height: 60
 	});
-	textWrap.add(label);
+	view.add(num);
+	view.add(label)
+	row.add(view);
+	tableData.push(row);
 }
 
-// Create the link to the Map
-var viewMap = Titanium.UI.createButton({
-	width:450,
-	height:55,
-	bottom:10,
-	enabled:true,
-	visible:true,
-	title:'View Map',
-});
+table.setData(tableData);
 
-// Add functionality
-viewMap.addEventListener('click', function() {	
-	var w = Ti.UI.createWindow({
-		title:"TCAT Route - Map",
-		url:"mapRoute.js",
-		from:userStartingPoint,
-		to:userDestination,
-		data:data
-	});
-	Titanium.UI.currentTab.open(w,{animated:true});
-});
-
-scrollView.add(textWrap);
-win.add(scrollView);
-win.add(viewMap);
+win.add(table);
